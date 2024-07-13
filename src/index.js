d24f2cb67b65f63ad8f57e03d2d7b6037712d673
Saurabh Singh <singh.saurabh2250@gmail.com>
2024-07-13 14:51:49 +0530

    associated airport and city and create airport using models method

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const { PORT }=require('./config/serverconfig');
 const apiRoutes=require('./routes/index');
 const {cityrepository}=require('./repository/index');
+const {City,Airport}=require('./models/index');
+const { where } = require("sequelize");
+const db = require('./models/index');
 
 
 const setupandstartserver = async() => {
@@ -16,6 +19,29 @@ const setupandstartserver = async() => {
 
     app.listen(PORT,async()=>{
         console.log(`server started at ${PORT}`);
+
+        // const city=await City.findOne({
+        //     where:{
+        //         id:4
+        //     }
+        // });
+        // const airport=await city.getAirports();
+        // let newAirport=await Airport.create({
+        //     name: 'Behala Airport',
+        //     cityId:8
+        // });
+
+        const city=await City.findOne({
+            where:{id : 4}
+        });
+        const newAirport=await Airport.findOne({
+            where:{id : 5}
+        });
+
+        console.log(city,newAirport);
+        await city.addAirport(newAirport);
+
+
     });
 }
 setupandstartserver();
